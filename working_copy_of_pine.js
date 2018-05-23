7aca0fbcf87d07490850c1d1789b68a09b8fa288
altD <35429528+PixelXII@users.noreply.github.com>
2018-05-23 14:07:40 -0700

    Create working_copy_of_pine.js

---
+++ working_copy_of_pine.js
@@ -0,0 +1,47 @@
+// Technical things -- getting the output in the right place, setting event listeners etc
+var output;
+var action;
+var inElement = document.getElementById('input');
+var outElement = document.getElementById('out');
+setInterval(printOut(output), 100)
+
+function printOut(mess) {
+  outElement.innerHTML = mess
+}
+
+function doAction() {
+	action = inElement.value;
+	printOut(action);
+}
+
+inElement.addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) {
+      setTimeout(doAction(), 500);
+    }
+});
+
+// The actual game
+
+var game = new Object();
+
+game.start = function() {
+	output = "WELCOME TO PINE!!!"
+	setTimeout(printOut("Pine is a text-based rpg, like <a href='https://zorkonline.net'>Zork</a>."), 5000);
+	setTimeout(printOut("Don't kill me for copying Zork."), 5000);
+	setTimeout(printOut("Enjoy the game! Type 'help' for help."), 500);
+	setTimeout(game.first, 5000);
+}
+		
+game.first = function() {}
+		   
+game.move = function() {
+    if(action.inlcudes('move') || action.includes('walk') || action.includes('step')) {
+		if(action.charAt(6) != ' ') {
+			var five = action.charAt(5)+action.charAt(6);
+        } else {
+        	var five = action.charAt(5);
+        }
+		
+        console.log('You move ' + five + ' steps ' + direction + '.');
+	}
+}
