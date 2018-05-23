e0e00a986c98303e3b616138a2e7d7d70be4efac
altD <35429528+PixelXII@users.noreply.github.com>
2018-05-22 20:55:59 -0700

    Update overdrive.js

--- overdrive.js
+++ overdrive.js
@@ -1,11 +1,14 @@
 var output;
 var action;
+var aElement = document.getElementById('input');
+
 function printOut(mess) {
   aElement.innerHTML = mess
 }
-var aElement = document.getElementById('input');
+
 function doAction() {
   aElement.submit;
+  console.log('done');
   action = document.getElementById('input').value;
   setInterval(printOut(action), 100)
 }
