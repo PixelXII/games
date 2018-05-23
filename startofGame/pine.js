ddf937e771b41b1308c18394a270362abc8dd5f6
altD <35429528+PixelXII@users.noreply.github.com>
2018-05-23 15:39:12 -0700

    Update pine.js

--- startofGame/pine.js
+++ startofGame/pine.js
@@ -14,8 +14,10 @@ function doAction() {
 	command = inElement.value;
 	if(command == 'start' || command == 'Start') {
 		game.start()
-	} else {
-		printOut(command)
+	} else if(command == 'help' || command == 'help') {
+		game.help()
+	}
+		
 	}
 	console.log('doAction() done');
 	} catch {
@@ -34,11 +36,18 @@ inElement.addEventListener("keydown", function (e) {
 var game = new Object();
 
 game.start = function() {
-	output = "WELCOME TO PINE!!!"
-	setTimeout(printOut("Pine is a text-based rpg, like <a href='https://zorkonline.net'>Zork</a>. Enjoy the game! Type 'help' for help."), 5000);
+	printOut("Pine is a text-based rpg, like <a href='https://zorkonline.net'>Zork</a>. Enjoy the game! Type 'help' for help.")
 }
 
-game.first = function() {}
+game.help = function() {
+	output = "The way this game works is you give me a command, and I will execute it. Try playing <a href='https://zorkonline.net'>Zork</a> to get a feel for the commands and the way the game works.");
+}
+
+game.first = function() {
+	output = "game written by kai wildberger"
+	setTimeout(function() { printOut('You are standing at the entrance to a tunnel which leads under a road. Behind you, to the north, is a trail leading to the nearby town.'); }, 5000);
+	setTimeout(function() { printOut('You can see a dried-up streambed on the other side of the tunnel.'); }, 5000);	
+}
 		   
 game.move = function() {
     if(command.inlcudes('move') || command.includes('walk') || command.includes('step')) {
