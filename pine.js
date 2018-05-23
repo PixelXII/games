546e3ec4e1e7768bd3c3d9749a9fbd8ed0bb3794
altD <35429528+PixelXII@users.noreply.github.com>
2018-05-23 14:17:39 -0700

    Update pine.js

--- pine.js
+++ pine.js
@@ -1,18 +1,17 @@
 // Technical things -- getting the output in the right place, setting event listeners etc
 var output = "pine"
-var action;
+var command;
 var inElement = document.getElementById('input');
 var outElement = document.getElementById('out');
-
+setInterval(printOut(output), 100)
 
 function printOut(mess) {
   outElement.innerHTML = mess
 }
 
 function doAction() {
-	action = inElement.value;
-	printOut(action);
-	setInterval(printOut(output), 100)
+	command = inElement.value;
+	printOut(command);
 }
 
 inElement.addEventListener("keydown", function (e) {
@@ -32,11 +31,11 @@ game.start = function() {
 game.first = function() {}
 		   
 game.move = function() {
-    if(action.inlcudes('move') || action.includes('walk') || action.includes('step')) {
-		if(action.charAt(6) != ' ') {
-			var five = action.charAt(5)+action.charAt(6);
+    if(command.inlcudes('move') || command.includes('walk') || command.includes('step')) {
+		if(command.charAt(6) != ' ') {
+			var five = command.charAt(5)+command.charAt(6);
         } else {
-        	var five = action.charAt(5);
+        	var five = command.charAt(5);
         }
 		
         console.log('You move ' + five + ' steps ' + direction + '.');
@@ -49,10 +48,8 @@ var game = new Object();
 
 game.start = function() {
 	output = "WELCOME TO PINE!!!"
-	setTimeout(printOut("Pine is a text-based rpg, like <a href='https://zorkonline.net'>Zork</a>."), 5000);
-	setTimeout(printOut("Don't kill me for copying Zork."), 5000);
-	setTimeout(printOut("Enjoy the game! Type 'help' for help."), 500);
-	setTimeout(game.first, 5000);
+	setTimeout(printOut("Pine is a text-based rpg, like <a href='https://zorkonline.net'>Zork</a>. Enjoy the game! Type 'help' for help."), 5000);
+	game.first()
 }
 		
 game.first = function() {}
