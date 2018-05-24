29dda68236f3fa8a19774351c27951c9f69574d3
altD <35429528+PixelXII@users.noreply.github.com>
2018-05-24 16:36:52 -0700

    Update pine.js

--- pine.js
+++ pine.js
@@ -96,7 +96,7 @@ inElement.addEventListener("keydown", function (e) {
 var game = new Object();
 
 game.start = function() {
-	setTimeout(printOut("Pine is a text-based RPG, like <a href='http://zorkonline.net'>Zork</a>. Have fun!"), 5000);
+	setTimeout(printOut("Pine is a text-based RPG, like <a href='http://zorkonline.net'>Zork</a>. <br> <h1>COMMANDS:</h1> <br> <br> 'move' to move <br> 'look' to look"), 5000);
 	setTimeout(function() {game.first()}, 5000);
 }
 
