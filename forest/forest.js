9847fe7dc6ff6a8c7bcce6283070562b47a95e95
altD <35429528+PixelXII@users.noreply.github.com>
2018-05-26 10:53:05 -0700

    Update forest.js

--- forest/forest.js
+++ forest/forest.js
@@ -20,10 +20,11 @@ function printOut(mess, mess2) {
 
 var searchArray = function(needle, haystack){
   for(var i = 0; i < needle.length; i++){
-    if(haystack.indexOf(needle[i]) === -1)
-       return false;
-  }
-  return true;
+    	if(haystack.indexOf(needle[i]) === -1) {
+       		return false;
+  	} else {
+ 	 	return true;
+	}
 }
 
 var slice = function(command, start, end) {
