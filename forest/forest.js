7aa3284584b5f5f54e427fa776849f0dafa6f899
altD <35429528+PixelXII@users.noreply.github.com>
2018-05-29 07:46:58 -0700

    Update forest.js

--- forest/forest.js
+++ forest/forest.js
@@ -362,7 +362,9 @@ game.start = function() {
 
 game.look = function(action, rightMess, leftMess, behMess, foMess) {
 	if(action.includes('look ') || action.includes('Look ')) {
-		
+		if(action == 'look' || action.includes('look') && action.includes('around')) {
+			printOut(rightMess+'<br>'+leftMess+'<br>'+behMess+'<br>'+foMess)
+		}
 		if(action.includes('right')) {
 			return rightMess;
 		} else if(action.includes('left')) {
