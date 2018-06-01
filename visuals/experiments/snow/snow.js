3f8bb2c38cf4691cd91efcd5104d39adafe094ec
altD <35429528+PixelXII@users.noreply.github.com>
2018-06-01 13:40:10 -0700

    Update snow.js

--- visuals/experiments/snow/snow.js
+++ visuals/experiments/snow/snow.js
@@ -14,15 +14,15 @@ setInterval(function(){ displayImage() }, 100) */
 window.onload = function() {
   var slideIndex = 0;
   var x = document.getElementsByClassName("mySlides");
-  var i;
+  var i = 0;
   function skooch() {
-      for (i = 0; i < x.length; i++) {
+      for (i < x.length; i++) {
         x[i].style.display = "none"; 
       }
       slideIndex++;
       if (slideIndex >= x.length) {slideIndex = 1} 
       x[slideIndex-1].style.display = "block"; 
   }
-  setInterval(skooch, 500);
+  setInterval(skooch, 100);
   skooch();
 }
