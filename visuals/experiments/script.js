813ab28d3b45d940f39e5d15b7373f83393522da
altD <35429528+PixelXII@users.noreply.github.com>
2018-05-31 20:04:38 -0700

    Update script.js

--- visuals/experiments/script.js
+++ visuals/experiments/script.js
@@ -3,3 +3,9 @@ var ctx = canvas.getContext("2d");
 canvas.width = 512;
 canvas.height = 480;
 document.getElementsByTagName('div').appendChild(canvas);
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function () {
+	bgReady = true;
+};
+bgImage.src = "./grass.png";
