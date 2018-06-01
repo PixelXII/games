1a2e66465522f15e1f26699653b479efdb0748e3
altD <35429528+PixelXII@users.noreply.github.com>
2018-06-01 09:26:24 -0700

    Update snow.js

--- visuals/experiments/snow/snow.js
+++ visuals/experiments/snow/snow.js
@@ -1,4 +1,4 @@
-var baller = new Image()
+/* var baller = new Image()
 var canvas = document.getElementById('canvas');
 var ball = document.getElementById('baller');
 
@@ -10,4 +10,19 @@ function displayImage() {
   }
 }
 
-setInterval(function(){ displayImage() }, 100)
+setInterval(function(){ displayImage() }, 100) */
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 500);
+}
