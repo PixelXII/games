59f96aaa94867e350dd84a0e0a5a96fd55012337
altD <35429528+PixelXII@users.noreply.github.com>
2018-06-01 08:00:29 -0700

    Update snow.js

--- visuals/experiments/snow/snow.js
+++ visuals/experiments/snow/snow.js
@@ -1,1 +1,13 @@
+var baller = new Image()
+var canvas = document.getElementById('canvas');
+canvas.appendChild(baller)
 
+var i = 1
+function displayImage() {
+  for(i<10) {
+    baller.src = i+'.png'
+    i++
+  }
+}
+
+setInterval(function(){ displayImage() }, 100)
