27b70a7c56c011622d707e651efd82b1c786261a
altD <35429528+PixelXII@users.noreply.github.com>
2018-06-12 07:41:41 -0700

    Update desert.js

--- text/desert/desert.js
+++ text/desert/desert.js
@@ -96,6 +96,34 @@ function firstPlace() {
 	}
 }
 
+function wellPlace() {
+	out2.innerHTML = ""
+	var eats = ['cactus fruit', 'well']
+	var poisons = ['']
+	var items = ['rock', 'cactus fruit', "sand", 'water']
+	eating(eats, items, poisons)
+	if(command.includes('look')) {
+	outElement.innerHTML = game.look(command,
+	 "On your right there ??? stillll inn development ?", 
+	 "To your left is the dune where you started.", 
+   	 "Behind you is the massive mound of boulders.",
+   	 "In front of you is the well itself, bucket and all.",
+	 "Above you is the awning over the well. It's made of rotting wood and old rusty nails.", 
+	 "Below you is the ground. You can see cactus fruit, rocks, sand, and the walls of the well."
+    )
+	}
+	if(command.includes('walk') || command.includes('step') || command.includes('move')) {
+  outElement.innerHTML = game.move(command, 
+		"You walk to the well.",
+								 "well",
+		"You slip and slide and eventually you find yourself at the bottom of the slope. <br> Gravel and sand continues falling long after you've reached the bottom.",
+								 "belowSlope",
+		"You cannot climb the boulders.",
+								 "first",
+		"Walking forward, you come to the end of the small dune.",
+								"dune")
+	}
+
 function doAction() {
 		command = inElement.value;
 		inElement.value = ""; 
