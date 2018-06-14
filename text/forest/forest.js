39894e63798362bb1265cb5ddc327a4b3cf203cf
altD <35429528+PixelXII@users.noreply.github.com>
2018-06-14 15:26:09 -0700

    Update forest.js

--- text/forest/forest.js
+++ text/forest/forest.js
@@ -48,6 +48,8 @@ function printOut(mess, mess2) {
 function notedis() {
 	if(place === 'insidePlace' || place === 'shrine' && inventory.contentsOf.includes('note')) {
 		noteElem.innerHTML = "<img src='./noote.png' style='height:50; width:50;'>"
+	}
+}
 
 var searchArray = function(needle, haystack) {
   for(var i = 0; i < needle.length; i++){
