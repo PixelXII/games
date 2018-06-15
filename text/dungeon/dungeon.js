d70c36d252d691f2d5885f8b50b9e096353f1c14
altD <35429528+PixelXII@users.noreply.github.com>
2018-06-14 21:33:38 -0700

    Update dungeon.js

--- text/dungeon/dungeon.js
+++ text/dungeon/dungeon.js
@@ -346,7 +346,7 @@ game.pickUp = function(action, eats, items, poisons) {
 			 }
 		 } else if(area.includes('male')) {
 			   if(inventory.contentsOf.includes(thing)) {
-			 	printOut("You hit the man with the " + thing ". You are at an advantage here. <br> You quickly knock him out with the " + thing + ".")
+			 	printOut("You hit the man with the " + thing ". You are at an advantage here and you quickly knock him out.")
 		 	   } else {
 				   printOut('You do not have a ' + thing)
 			   }
