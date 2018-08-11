c59cd62adc25d1d1076f087778b8f2890e6131b3
altD <35429528+PixelXII@users.noreply.github.com>
2018-08-11 08:39:44 -0700

    Update sol.js

--- visuals/alagaesia/sol.js
+++ visuals/alagaesia/sol.js
@@ -1,1 +1,8 @@
-
+function Spell(name, type, damage, img) {
+  this.name = name
+  this.spellType = type
+  this.damage = damage
+  
+  document.getElementById('tb').innerHTML = document.getElementById('tb').innerHTML + '<td> <img class=\'spell\' src="'+img+'"> </td>'
+}
+  
