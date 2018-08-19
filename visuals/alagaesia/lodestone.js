97fc1a3b32a42df0656b83b292c6a36d79f9ce4f
altD <35429528+PixelXII@users.noreply.github.com>
2018-08-19 16:57:54 -0700

    added playername checks

--- visuals/alagaesia/lodestone.js
+++ visuals/alagaesia/lodestone.js
@@ -5,15 +5,26 @@ function firstLoad() {
   var div = document.createElement('div')
   document.body.appendChild(div)
   div.id = 'startup'
-  div.innerHTML = "<div align='center'> <br> <img src='images/logo.png'/> <br> <br> <br> <div id='title' class='largetext'>Spellfyre</div> <br> <br> <legend>What is your name?</legend> <br> <input class='input' id='playername' type='text'> <br> <br> <br> <div id='enter' class='button' style='background-color:#3f51b5; width:100px;'>!&nbsp ENTER</div><br>"
+  div.innerHTML = "<div align='center'> <br> <img src='images/logo.png'/> <br> <br> <br> <div id='title' class='largetext'>Spellfyre</div> <br> <br> <legend>What is your name?</legend> <br> <input class='input' id='playername' type='text'> <br> <legend id='warn'></legend> <br> <br> <div id='enter' class='button' style='background-color:#3f51b5; width:100px;'>!&nbsp ENTER</div><br>"
   id('main').style.display = 'none'
   id('enter').addEventListener('click', function() {
+    playername = id('playername').value
+    id('playername').value = ''
+    if(playername === '' || playername.length <= 2) {
+      id('warn').style.color = 'red'
+      id('warn').innerHTML = 'Please choose a longer name'
+    }
+    playerdata.name = playername
     enter()
   });
   id('playername').addEventListener('keypress', function(e) {
     if(e.keyCode === 13) {
       playername = id('playername').value
       id('playername').value = ''
+      if(playername === '' || playername.length <= 2) {
+        id('warn').style.color = 'red'
+        id('warn').innerHTML = 'Please choose a longer name'
+      }
       playerdata.name = playername
       enter()
     }
