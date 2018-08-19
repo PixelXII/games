2ed1f2c5a83144df542d7996f67229d736cb0cdd
altD <35429528+PixelXII@users.noreply.github.com>
2018-08-18 21:19:41 -0700

    first commit

--- visuals/alagaesia/lodestone.js
+++ visuals/alagaesia/lodestone.js
@@ -1,1 +1,26 @@
+var playername;
 
+function firstLoad() {
+  var div = document.createElement('div')
+  document.body.appendChild(div)
+  div.id = 'startup'
+  div.innerHTML = "<div align='center'> <br> <img src='images/logo.png'/> <br> <br> <br> <div class='largetext'>$GAME_NAME</div> <br> <br> <legend>What is your name?</legend> <br> <input id='playername' type='text'> <br> <br> <div id='enter' class='button' style='background-color:#3f51b5;'>!&nbsp ENTER</div><br>"
+  id('main').style.display = 'none'
+  id('enter').addEventListener('click', function() {
+    enter()
+  });
+  id('playername').addEventListener('keypress', function(e) {
+    if(e.keyCode === 13) {
+      playername = id('playername').value
+      id('playername').value = ''
+      enter()
+    }
+  });
+}
+
+function enter() {
+  id('startup').style.display = 'none'
+  id('main').style.display = 'none'
+}
+
+firstLoad()
