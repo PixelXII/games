eab9d68b2f0c264b11f1a188098d41182589f887
altD <35429528+PixelXII@users.noreply.github.com>
2018-09-13 21:42:25 -0700

    Update area.js

--- text/arkeus/js/area.js
+++ text/arkeus/js/area.js
@@ -1,3 +1,4 @@
+var ie = document.getElementById('ie')
 var nwS = function(val) {
     Game.lookForward = 'Ahead of you is a road leading to a small village.'
     Game.lookBack = 'Behind you is the tunnel entrance, now sealed over with a strong iron grate.'
@@ -48,8 +49,62 @@ var barties = function(val) {
      Game.lookLeft = 'To your left is a small stairway leading up to the second story, where the inn\'s guests sleep.'
      Game.lookDown = 'Below you are the floorboards of Bartie\'s Food and Brew. They are smooothed with use, and you can see stains and dropped trinkets between the cracks.'
      Game.lookUp = "Above, the you see the rafters of the inn."
-     var barties = new Shop('Bartie\'s Food and Brew', 'Bartie', [HealingTea, Ale, Beer, Wine], [10, 10, 15, 20])
-     Game.location.shop = barties
+     Game.moveBack = 'You walk back outside to the road.'
+     Game.back = 'outskirts'
+     Game.moveRight = 'You walk through the doorway and enter the bar area. There is one man at the bar, and he looks sad and worried about something.'
+     Game.right = 'barties-bar'
+     Game.moveLeft = 'You trudge up the stairs to the guests\' rooms.'
+     Game.left = 'barties-inn'
+     var bartie = new Shop('Bartie\'s Food and Brew', 'Bartie', [HealingTea, Ale, Beer, Wine], [10, 10, 15, 20])
+     Game.location.shop = bartie
      
      Game.auto(val)
+     barties = function(val) {
+          Game.lookBack = 'The door to the road outside is closed.'
+          Game.auto(val)
+     }
+}
+
+var bBar = function(val) {
+     Game.location.items = []
+     var Jasper = new Person('Jasper', function() {
+          consul.dialogue('Jasper says: "Are you here to help me?"')
+          setTimeout(function() {
+               document.getElementById('ie').disabled = true
+               consul.dialogue('Jasper says: "Some damn critters are eating my crops. I live just towards town and across the river."')
+               setTimeout(function() {
+                    consul.dialogue('Jasper says: "I need someone to kill the creatures for me. I can\'t do it myself."')
+                    setTimeout(function() {
+                         consul.dialogue('Jasper thanks you for your help.')
+                         var jaspersFarm = new Quest("Jasper's Damn Creatures", 'Jasper wants you to help him restore his farm by killing whatever creatures are eating his crops.', function() {
+                              consul.dialogue('Jasper says: "It\'s not much, but it\'s what I have. I hope it\'s enough."')
+                              consul.info('Jasper gives you a handful of coins.')
+                              consul.dialogue('Jasper says: "You don\'t know how much this means to me. My farm can function again!"')
+                              Player.gold.amount += 20
+                         })
+                         jaspersFarm.initiate()
+                         setTimeout(function() {
+                              document.getElementById('ie').disabled = false
+                              consul.dialogue('Jasper says: "Thank you for doing this."')
+                              Jasper.dialogue = function() {
+                                   consul.dialogue('Jasper says: "Thank you for doing this."')
+                              }
+                         }, 4000)
+                    }, 5000)
+               }, 6000)
+          }, 5500)
+     })
+     Game.location.person = Jasper
+     Game.lookForward = 'In front of you is the bar and the man sitting at it.'
+     Game.lookLeft = 'To your left is a small cluster of tables. It looks like there was a drinking competition recently, as there are plenty of stains on the floor, and not just from ale.'
+     Game.lookRight = 'To your right is the wall of the inn.'
+     Game.lookBack = 'Behind you is the lobby where Bartie sells his wares.'
+     Game.moveBack = 'You turn around and head back through the door to the lobby.'
+     Game.back = 'barties'
+     Game.forward = 'barties-bar'
+     Game.left = Game.forward
+     Game.right = Game.left
+     
+     Game.auto(val)
+     
 }
