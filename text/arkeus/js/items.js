ebe31e0c9e217ba6bc3da4d539bf329d724e0311
altD <35429528+PixelXII@users.noreply.github.com>
2018-09-14 23:23:18 -0700

    Update items.js

--- text/arkeus/js/items.js
+++ text/arkeus/js/items.js
@@ -13,3 +13,26 @@ var Beer = new Item('beer', 'It\'s classic beer.', 25, true, function() { Player
 var Wine = new Item('wine', 'It\'s your average wine.', 50, true, function() { Player.hp += 25; Game.health(); })
 var Tankard = new Item('wine', 'It smells awful.', 0, false, function() {})
 var Mug = new Item('mug', 'It smells like beer.', 0, false, function() {})
+
+var sword = 'sword'
+var mace = 'mace'
+var axe = 'axe'
+var spear = 'spear'
+
+var IronSword = new Weapon(sword, 'Iron Sword', 13, 'A simple iron sword.')
+var SteelSword = new Weapon(sword, "Steel Sword", 15, 'A simple steel sword.')
+var AdamantineSword = new Weapon(sword, "Adamantine Sword", 16, 'The adamantine in the sword glows with a soft red light.')
+var IronMace = new Weapon(mace, "Iron Mace", 17, 'A simple iron mace.')
+var SteelMace = new Weapon(mace, "Steel Mace", 11, 'A simple steel mace.')
+var EbonyMace = new Weapon(mace, "Ebony Mace", 12, 'This mace seems to absorb all light around it.')
+var WoodAxe = new Weapon(axe, "Wooden War Axe", 14, 'A primitive war axe.')
+var IronAxe = new Weapon(axe, "Iron War Axe", 12, 'A simple iron war axe.')
+var SteelAxe = new Weapon(axe, "Steel War Axe", 12, 'A simple steel war axe.')
+var IronSpear = new Weapon(spear, 'Iron Spear', 11, 'A simple iron spear.')
+var SteelSpear = new Weapon(spear, 'Steel Spear', 13, 'A simple steel spear.')
+var OrichalcumSpear = new Weapon(spear, 'Orichalcum Spear', 15, 'The orichalcum in the spear gives off a light without color.')
+
+var CleodianDagger = new Weapon('dagger', 'Cleodian Dagger', 20, 'Whoever wielded this dagger last must have had immense strength, as the dagger is very heavy.')
+var RoransHammer = new Weapon('hammer', 'Roran\'s Hammer', 19, 'The hammer looks like a blacksmith\'s hammer.')
+var NocriteKatana = new Weapon('katana', 'Nocrite Katana', 22, 'The katana gleams in the light of the sun, giving off a warm glow.')
+var StiraothsStaff = new Weapon(sword, "Stiraoth's Quarterstaff", 21, 'The previous owner of this staff was either very rich or had a lot of skill with woodcarving, because the staff has many intricate carvings. Some depict stories of battles, and others are just designs.')
