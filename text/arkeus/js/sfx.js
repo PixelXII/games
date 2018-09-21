b036f090983b743a0a86c66e92177e70c41a3284
altD <35429528+PixelXII@users.noreply.github.com>
2018-09-21 10:32:12 -0700

    Update sfx.js

--- text/arkeus/js/sfx.js
+++ text/arkeus/js/sfx.js
@@ -1,4 +1,6 @@
 var sounds = {
      consume: new Audio('assets/consume.mp3'),
-     equip: new Audio('assets/equip.mp3')
+     equip: new Audio('assets/equip.mp3'),
+     atk: new Audio('assets/attack.mp3'),
+     coin: new Audio('assets/coins.mp3')
 }
