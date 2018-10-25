b3c87d593d386e015ebfebf3697e1a52f896c938
altD <35429528+PixelXII@users.noreply.github.com>
2018-10-25 11:22:41 -0700

    Update main.js

--- text/arkeus/js/main.js
+++ text/arkeus/js/main.js
@@ -1,11 +1,27 @@
 // global
 var consuul = consul.create(new Element('div', document.body), '650', 'calc(100%-30)', 'black', '#0097a7', '#1565c0', '#ffffff')
-var ti = consuul.title(`<img src="assets/ark.png" width='20' height='20' style="padding-right:6px;">Arkeus`)
+var ti = consuul.title(`<img alt="logo"src="assets/ark.png" width='20' height='20' style="padding-right:6px;">Arkeus<span style="font-size:18px; float:right; margin-right:8px; margin-top:4px;" class="setting" id="mute">Mute</span>`)
+var style = document.createElement('style')
+document.body.appendChild(style)
+style.innerHTML = `.setting {text-decoration:underline; cursor:pointer;} .setting:hover {text-decoration:none;}`
+document.getElementById('mute').addEventListener('click', () => {
+     if(Game.muted) {
+          Game.unmute()
+          document.getElementById('mute').innerHTML = 'Mute'
+     } else {
+          Game.mute()
+          document.getElementById('mute').innerHTML = 'Unmute'
+     }
+})
 ti.style.borderBottomStyle = 'double'
 var inputstyle = consul.input(parse, true)
 inputstyle.id = 'ie'
 inputstyle.style.width = '100%'
 document.querySelector('.consul-element').style.height = window.innerHeight - inputstyle.height - 110;
+window.onresize = function() {
+     document.querySelector('.consul-element').style.height = window.innerHeight - inputstyle.height - 110;
+     document.querySelector('.consul-element').style.width = window.innerWidth - 40;
+}
 consul.dialogue('You are standing on a small trail, just outside the village of Rivergate.')
 document.body.style.height = null
 var Player = {
@@ -16,7 +32,7 @@ var Player = {
     inventory: [],
     quests: [],
     inCombat: false,
-    gold: new Gold(500)
+    gold: new Gold(150)
 }
 
 Game.combatElement = document.getElementById('ie')
@@ -48,27 +64,39 @@ function parse(val) {
                     Game.location.opponent.hp = 0;
                     Player.inCombat = false
                     Game.location.opponent.dead = true;
-                    consul.info('The ' + Game.location.opponent.name + ' is dead.')
+                    consul.info(`The ${Game.location.opponent.name} is dead.`)
                }
           }
           return false;
      }
+     a = val
         val = clean(val.toLowerCase())
         if(commands.includes(first(val))) {
              consul.log(Game.placeholder)
-             consul.log('> '+val)
-            if(Player.location === 'north-rivergate') {
-                 nRiver(val)
-            } else if(Player.location === 'rivergate.outskirts') {
-                 Rivergate.outskirts(val)
-            } else if(Player.location === 'rivergate.town') {
-                 Rivergate.town(val)
-            } else if(Player.location === 'rivergate.market') {
-                 Rivergate.market(val)
+             consul.log(`> ${val}`)
+            switch(Player.location) {
+               case 'north-rivergate':
+                    nRiver(val)
+                    break;
+               case 'rivergate.outskirts':
+                    Rivergate.outskirts(val)
+                    break;
+               case 'rivergate.town':
+                    Rivergate.town(val)
+                    break;
+               case 'rivergate.market':
+                    Rivergate.market(val)
+                    break;
+               case 'rivergate.alley':
+                    Rivergate.alley(val)
+                    break;
+               case 'rivergate.edge':
+                    Rivergate.edge(val)
+                    break;
             }
         } else {
             if(val !== '') {
-                consul.error('"'+val + '"'+' is not a command. Type "help" for a list of commands.')
+                consul.error('"'+a + '"'+' is not a command. Type "help" for a list of commands.')
             }
         }
 }
