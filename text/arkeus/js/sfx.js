2dc607a5d6f3239c201014876b7235bb6e242c9e
altD <35429528+PixelXII@users.noreply.github.com>
2018-11-11 08:04:20 -0800

    Update sfx.js

--- text/arkeus/js/sfx.js
+++ text/arkeus/js/sfx.js
@@ -1,6 +1,6 @@
 var sounds = {
-     consume: new Audio('assets/consume.mp3'),
-     equip: new Audio('assets/equip.mp3'),
-     atk: new Audio('assets/attack.mp3'),
-     coin: new Audio('assets/coins.mp3')
+     consume: new Audio('https://cdn.jsdelivr.net/gh/PixelXII/games@1.2.8/text/arkeus/assets/consume.mp3'),
+     equip: new Audio('https://cdn.jsdelivr.net/gh/PixelXII/games@1.2.8/text/arkeus/assets/equip.mp3'),
+     atk: new Audio('https://cdn.jsdelivr.net/gh/PixelXII/games@1.2.8/text/arkeus/assets/attack1.mp3'),
+     coin: new Audio('https://cdn.jsdelivr.net/gh/PixelXII/games@1.2.8/text/arkeus/assets/coins.mp3')
 }
